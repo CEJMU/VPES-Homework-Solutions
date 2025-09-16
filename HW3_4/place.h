5e4fc21b339e35c7cf3a4d323924bbcb03f9e038
Derek Christ <derek.christ@uni-wuerzburg.de>
2025-09-16 14:39:20 +0200

    Add HW3.1 solution

--- HW3_4/place.h
+++ HW3_4/place.h
@@ -3,7 +3,35 @@
 #include <systemc>
 
 // Place Interface:
-// TODO
+class placeInterface : public virtual sc_core::sc_interface
+{
+public:
+    virtual void addTokens(unsigned int n) = 0;
+    virtual void removeTokens(unsigned int n) = 0;
+    virtual unsigned int testTokens() = 0;
+};
 
 // Place Channel:
-// TODO
+class place : public placeInterface
+{
+public:
+    place(unsigned int tokens) : tokens(tokens) {}
+
+    void addTokens(unsigned int n) override
+    {
+        tokens += n;
+    }
+
+    void removeTokens(unsigned int n) override
+    {
+        tokens -= n;
+    }
+
+    unsigned int testTokens() override
+    {
+        return tokens;
+    }
+
+private:
+    unsigned int tokens;
+};
--- HW3_4/transition.h
+++ HW3_4/transition.h
@@ -5,4 +5,25 @@
 #include <systemc>
 
 // Transition:
-// TODO
+SC_MODULE(transition)
+{
+public:
+    SC_CTOR(transition) {}
+
+    sc_core::sc_port<placeInterface> in; 
+    sc_core::sc_port<placeInterface> out;
+
+    void fire()
+    {
+        if (in->testTokens() >= 1)
+        {
+            std::cout << this->name() << ": Fired\n";
+            in->removeTokens(1);
+            out->addTokens(1);
+        }
+        else
+        {
+            std::cout << this->name() << ": NOT Fired\n";
+        }
+    }
+};
