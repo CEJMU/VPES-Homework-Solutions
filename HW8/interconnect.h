2a63c9e56229162f966c64daeea02ae360b8d45b
Derek Christ <derek.christ@uni-wuerzburg.de>
2026-02-05 16:17:29 +0100

    Add HW8 solution

--- HW8/interconnect.h
+++ HW8/interconnect.h
@@ -1,8 +1,25 @@
 #pragma once
 
+#include <format>
+#include <iostream>
 #include <tlm>
 #include <tlm_utils/multi_passthrough_initiator_socket.h>
 #include <tlm_utils/multi_passthrough_target_socket.h>
+
+struct RoutingExtension : public tlm::tlm_extension<RoutingExtension>
+{
+    int inIdx{};
+    int outIdx{};
+
+    [[nodiscard]] tlm_extension_base* clone() const override { return new RoutingExtension(*this); }
+
+    void copy_from(const tlm_extension_base& ext) override
+    {
+        const auto& cpyFrom = dynamic_cast<const RoutingExtension&>(ext);
+        inIdx = cpyFrom.inIdx;
+        outIdx = cpyFrom.outIdx;
+    }
+};
 
 SC_MODULE(Interconnect)
 {
@@ -10,7 +27,104 @@ public:
     tlm_utils::multi_passthrough_target_socket<Interconnect> tSocket;
     tlm_utils::multi_passthrough_initiator_socket<Interconnect> iSocket;
 
-    SC_CTOR(Interconnect){}
+    Interconnect(const sc_core::sc_module_name& name) :
+        sc_core::sc_module(name),
+        tSocket("tSocket"),
+        iSocket("iSocket")
+    {
+        tSocket.register_nb_transport_fw(this, &Interconnect::nb_transport_fw);
+        iSocket.register_nb_transport_bw(this, &Interconnect::nb_transport_bw);
+    }
 
-    // Your code here...
+private:
+    static int routeFW(int inPort, tlm::tlm_generic_payload& trans)
+    {
+        int outPort = 0;
+
+        // Memory map implementation:
+        if (trans.get_address() < 512)
+        {
+            outPort = 0;
+        }
+        else if (trans.get_address() >= 512 && trans.get_address() < 1024)
+        {
+            // Correct Address:
+            trans.set_address(trans.get_address() - 512);
+            outPort = 1;
+        }
+        else
+        {
+            trans.set_response_status(tlm::TLM_ADDRESS_ERROR_RESPONSE);
+        }
+
+        auto* ext = new RoutingExtension;
+        ext->inIdx = inPort;
+        ext->outIdx = outPort;
+        trans.set_auto_extension(ext);
+
+        std::cout << "\033[1;36m"
+                  << std::format("(E) @{:>12}: Extension Created (inPort = {:>2} outPort = {:>2})",
+                                 sc_core::sc_time_stamp().to_string(),
+                                 inPort,
+                                 outPort)
+                  << "\033[0m\n";
+
+        return outPort;
+    }
+
+    virtual tlm::tlm_sync_enum nb_transport_fw(
+        int id, tlm::tlm_generic_payload& trans, tlm::tlm_phase& phase, sc_core::sc_time& delay)
+    {
+        int outPort = 0;
+
+        if (phase == tlm::BEGIN_REQ)
+        {
+            // In the case of nb_transport_fw the address attribute is valid
+            // immediately upon entering the function but only when the phase
+            // is BEGIN_REQ. Following the return from any forward path TLM-2.0
+            // interface method call, the address attribute will have the value
+            // set by the interconnect component lying furthest downstream, and
+            // so should be regarded as being undefined for the purposes of
+            // transaction routing.
+            trans.acquire();
+
+            // Modify address accoring to memory map:
+            outPort = routeFW(id, trans);
+        }
+        else if (phase == tlm::END_RESP)
+        {
+            // Adress was already modified in BEGIN_REQ phase:
+            RoutingExtension* ext = nullptr;
+            trans.get_extension(ext);
+            outPort = ext->outIdx;
+            trans.release();
+        }
+        else
+        {
+            SC_REPORT_FATAL(name(), "Illegal phase received by initiator");
+        }
+
+        std::cout << "\033[1;37m"
+                  << std::format(
+                         "({})@{:>12}: Addr = {:08d}  inPort = {:>2} outPort = {:>2} ptr = {}",
+                         name(),
+                         sc_core::sc_time_stamp().to_string(),
+                         trans.get_address(),
+                         id,
+                         outPort,
+                         static_cast<void*>(&trans))
+                  << "\033[0m\n";
+
+        return iSocket[outPort]->nb_transport_fw(trans, phase, delay);
+    }
+
+    virtual tlm::tlm_sync_enum nb_transport_bw(
+        int id, tlm::tlm_generic_payload& trans, tlm::tlm_phase& phase, sc_core::sc_time& delay)
+    {
+        RoutingExtension* ext = nullptr;
+        trans.get_extension(ext);
+        int inPort = ext->inIdx;
+
+        return tSocket[inPort]->nb_transport_bw(trans, phase, delay);
+    }
 };
--- HW8/target.h
+++ HW8/target.h
@@ -33,16 +33,212 @@ private:
     static constexpr unsigned ACCEPT_DELAY_NS = 10;
     static constexpr unsigned INTERNAL_DELAY_NS = 40;
 
+    bool responseInProgress = false;
+    tlm::tlm_generic_payload* endRequestPending = nullptr;
+    tlm_utils::peq_with_cb_and_phase<Target> peq;
     unsigned int numberOfTransactions = 0;
     unsigned int bufferSize;
+    std::queue<tlm::tlm_generic_payload*> responseQueue;
 
 public:
     Target(const sc_core::sc_module_name& name, unsigned int bufferSize) :
         sc_module(name),
         tSocket("tSocket"),
+        peq(this, &Target::peqCallback),
         bufferSize(bufferSize)
     {
+        tSocket.register_nb_transport_fw(this, &Target::nb_transport_fw);
     }
 
-    // Your code here...
+    void printBuffer(unsigned int max, unsigned int n)
+    {
+        std::string filled;
+        for (std::size_t i = 0; i < n; i++)
+            filled += "█";
+
+        const std::string empty(max - n, ' ');
+
+        std::cout << "\033[1;35m"
+                  << std::format("({})@{:>12}: Target Buffer: [{}{}] ( {} / {} )",
+                                 name(),
+                                 sc_core::sc_time_stamp().to_string(),
+                                 filled,
+                                 empty,
+                                 n,
+                                 max)
+                  << "\033[0m\n";
+    }
+
+    // [1.0, 1.6]
+    virtual tlm::tlm_sync_enum nb_transport_fw(
+        tlm::tlm_generic_payload & trans, tlm::tlm_phase & phase, sc_core::sc_time & delay)
+    {
+        // Queue the transaction into the peq until
+        // the annotated time has elapsed
+        peq.notify(trans, phase, delay);
+
+        return tlm::TLM_ACCEPTED; // [1.1, 1.7, (1.8)]
+    }
+
+    void peqCallback(tlm::tlm_generic_payload & trans, const tlm::tlm_phase& phase)
+    {
+        if (phase == tlm::BEGIN_REQ) // [1.0]
+        {
+            // Increment the transaction reference count
+            trans.acquire();
+
+            if (numberOfTransactions < bufferSize) // Input buffersize
+            {
+                sendEndRequest(trans); // [1.2]
+                // HINT: instead of [1.2] we can call also [4.1] (ie. [1.4])
+            }
+            else // If buffer is full we do backpressure
+            {
+                // Put back-pressure on initiator by deferring END_REQ
+                endRequestPending = &trans;
+            }
+        }
+        else if (phase == tlm::END_RESP) // [1.6]
+        {
+            // On receiving END_RESP, the target can release the transaction
+            // and allow other pending transactions to proceed
+
+            if (!responseInProgress)
+            {
+                SC_REPORT_FATAL(name(), "Illegal transaction phase END_RESP received by target");
+            }
+
+            // Reduce number of transactions in target:
+            numberOfTransactions--;
+            printBuffer(bufferSize, numberOfTransactions);
+
+            // Target itself is now clear to issue the next BEGIN_RESP
+            responseInProgress = false;
+            if (responseQueue.size() > 0)
+            {
+                tlm::tlm_generic_payload* next = responseQueue.front();
+                responseQueue.pop();
+                sendResponse(*next);
+            }
+
+            // ... and to unblock the initiator by issuing END_REQ
+            if (endRequestPending != nullptr)
+            {
+                sendEndRequest(*endRequestPending);
+                endRequestPending = nullptr;
+            }
+        }
+        else if (phase == INTERNAL)
+        {
+            // Execute the read or write commands
+            executeTransaction(trans);
+
+            // Target must honor BEGIN_RESP/END_RESP exclusion rule
+            // i.e. must not send BEGIN_RESP until receiving previous
+            // END_RESP or BEGIN_REQ
+            if (responseInProgress)
+            {
+                responseQueue.push(&trans);
+            }
+            else
+            {
+                sendResponse(trans);
+            }
+        }
+        else // tlm::END_REQ or tlm::BEGIN_RESP
+        {
+            SC_REPORT_FATAL(name(), "Illegal transaction phase received");
+        }
+    }
+
+    void sendEndRequest(tlm::tlm_generic_payload & trans)
+    {
+        // Queue the acceptance and the response with the appropriate latency
+        tlm::tlm_phase bw_phase = tlm::END_REQ;
+        sc_core::sc_time delay = sc_core::sc_time(ACCEPT_DELAY_NS, sc_core::SC_NS);
+
+        tlm::tlm_sync_enum status = tSocket->nb_transport_bw(trans, bw_phase, delay); // [1.2]
+        // Ignore return value (has to be TLM_ACCEPTED anyway)
+        // initiator cannot terminate transaction at this point
+
+        // Queue internal event to mark beginning of response
+        delay = delay + sc_core::sc_time(INTERNAL_DELAY_NS, sc_core::SC_NS);
+        peq.notify(trans, INTERNAL, delay);
+
+        numberOfTransactions++;
+        printBuffer(bufferSize, numberOfTransactions);
+    }
+
+    void executeTransaction(tlm::tlm_generic_payload & trans)
+    {
+        tlm::tlm_command cmd = trans.get_command();
+        sc_dt::uint64 adr = trans.get_address();
+        unsigned char* ptr = trans.get_data_ptr();
+        unsigned int len = trans.get_data_length();
+        unsigned char* byt = trans.get_byte_enable_ptr();
+        unsigned int wid = trans.get_streaming_width();
+
+        if (trans.get_address() >= MEMORY_SIZE)
+        {
+            trans.set_response_status(tlm::TLM_ADDRESS_ERROR_RESPONSE);
+            return;
+        }
+        if (byt != nullptr)
+        {
+            trans.set_response_status(tlm::TLM_BYTE_ENABLE_ERROR_RESPONSE);
+            return;
+        }
+        if (len > 4 || wid < len)
+        {
+            trans.set_response_status(tlm::TLM_BURST_ERROR_RESPONSE);
+            return;
+        }
+
+        if (cmd == tlm::TLM_READ_COMMAND)
+        {
+            memcpy(&mem[trans.get_address()], // destination
+                   trans.get_data_ptr(),      // source
+                   trans.get_data_length());  // size
+        }
+        else if (cmd == tlm::TLM_WRITE_COMMAND)
+        {
+            memcpy(trans.get_data_ptr(),      // destination
+                   &mem[trans.get_address()], // source
+                   trans.get_data_length());  // size
+        }
+
+        std::cout << "\033[1;32m"
+                  << std::format("({})@{:>12}: Exec. {} Addr = {:08d} Data = 0x{:08X}",
+                                 name(),
+                                 sc_core::sc_time_stamp().to_string(),
+                                 cmd == tlm::TLM_WRITE_COMMAND ? "Write" : "Read",
+                                 adr,
+                                 *reinterpret_cast<const int*>(ptr))
+                  << "\033[0m\n";
+
+        trans.set_response_status(tlm::TLM_OK_RESPONSE);
+    }
+
+    void sendResponse(tlm::tlm_generic_payload & trans)
+    {
+        sc_assert(responseInProgress == false);
+        responseInProgress = true;
+
+        tlm::tlm_phase bw_phase = tlm::BEGIN_RESP;
+        sc_core::sc_time delay = sc_core::SC_ZERO_TIME;
+        tlm::tlm_sync_enum status = tSocket->nb_transport_bw(trans, bw_phase, delay); // [1.4]
+
+        if (status == tlm::TLM_UPDATED) // [2.1]
+        {
+            // The timing annotation must be honored
+            peq.notify(trans, bw_phase, delay);
+        }
+        else if (status == tlm::TLM_COMPLETED)
+        {
+            SC_REPORT_FATAL(name(), "This transition is deprecated since TLM2.0.1");
+        }
+        // In the case of TLM_ACCEPTED [1.5] we will recv. a FW call [1.6]
+
+        trans.release();
+    }
 };
